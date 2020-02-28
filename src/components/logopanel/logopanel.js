95f2626d917f2bd01bc7d05ceb523c8bdd24990c
John Lee <jlee14c@hotmail.com>
2020-02-28 02:56:31 -0800

    hover nav prototype

--- src/components/logopanel/logopanel.js
+++ src/components/logopanel/logopanel.js
@@ -1,12 +1,32 @@
 import './index.less';
-import React from 'react';
+import React, {useState} from 'react';
 import MethaChartLogo from '../../assets/methachart_logo.png';
 import {navigate} from '../../utils/history';
 
-function LogoPanel() {
+function LogoPanel({onMouseOver, onMouseLeave}) {
+  const [hover, setHover] = useState(false);
+
+  function handleMouseOver() {
+    setHover(true);
+
+    if (onMouseOver) {
+      onMouseOver();
+    }
+  }
+
+  function handleMouseLeave() {
+    setHover(false);
+
+    if (onMouseLeave) {
+      onMouseLeave();
+    }
+  }
+
   return (
     <div
       className="logopanel-container"
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
       onClick={() => navigate('/chart/methadone')}>
       <img
         src={MethaChartLogo}
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,27 @@
 import './index.less';
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from '@reach/router';
 import { LogoPanel } from '..';
 import {Icon} from 'semantic-ui-react';
+
+function HoverNav({hover, onMouseEnter, onMouseLeave}) {
+  return (
+    <div
+      onMouseLeave={onMouseLeave}
+      onMouseEnter={onMouseEnter}
+      className={`navbar-hover-nav ${hover ? 'hover' : ''}`}>
+      <div className="nav-mock-logo-container">
+        <div className="nav-mock-logo logo-1">A</div>
+      </div>
+      <div className="nav-mock-logo-container">
+      <div className="nav-mock-logo logo-2">R</div>
+      </div>
+      <div className="nav-mock-logo-container">
+        <div className="nav-mock-logo logo-3">N</div>
+      </div>
+    </div>
+  )
+}
 
 function NavBarButton({label, to}) {
   return (
@@ -19,9 +38,24 @@ function NavBarIconButton({name, to}) {
 }
 
 function NavBar() {
+  const [hover, setHover] = useState(false);
+
+  function handleMouseOver() {
+    setHover(true);
+  }
+
+  function handleMouseLeave() {
+    console.log('leaving');
+    setHover(false);
+  }
+
   return (
     <div className="navbar-container">
-      <LogoPanel />
+      <HoverNav hover={hover}
+                onMouseLeave={handleMouseLeave}/>
+      <LogoPanel
+        onMouseOver={handleMouseOver}
+      />
       <div className="navbar-inner-container-left">
         {/*<NavBarButton to="chart/methadone" label="methadone" />*/}
         {/*<NavBarButton to="chart/suboxone" label="suboxone" />*/}
